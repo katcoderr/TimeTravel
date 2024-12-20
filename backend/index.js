40548b57c8ac4c70e51ddd600205e701334e951f
Kartik <kkartiktummawar@gmail.com>
2024-12-20 10:04:04 +0530

    Delete Image API

--- backend/index.js
+++ backend/index.js
@@ -195,6 +195,38 @@ app.post("/image-upload", upload.single("image") , async (req,res)=>{
     }
 })
 
+app.delete("/delete-image", async(req,res)=>{
+    const {imageUrl} = req.query
+    if(!imageUrl){
+        return res.status(400).json({
+            error : true,
+            message : "imageUrl parameter is required"
+        })
+    }
+
+    try{
+        const filename = path.basename(imageUrl)
+        const filePath = path.join(__dirname, 'uploads', filename)
+
+        if(fs.existsSync(filePath)){
+            fs.unlinkSync(filePath)
+            res.status(200).json({
+                message : "Image Deleted Successfully"
+            })
+        } else {
+            res.status(200).json({
+                error : true,
+                message : "Image not found"
+            })
+        }
+    } catch (error){
+        res.status(500).json({
+            error : true,
+            messsage : error.message
+        })
+    }
+})
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 
