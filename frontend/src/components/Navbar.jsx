25d10178f16e0d8e28e709249728909767ca9295
Kartik <kkartiktummawar@gmail.com>
2025-01-04 23:50:21 +0530

    Added Logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,7 +28,8 @@ const Navbar = ({ userInfo,
 
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow sticky top-0 z-10">
-      <h2 className="text-cyan-600">TimeTravel</h2>
+      {/* <h2 className="text-cyan-600">TimeTravel</h2> */}
+      <img src="./src/timetravellogo.png" alt="TimeTravel" width={164} />
 
       {isToken && <>
         <SearchBar
