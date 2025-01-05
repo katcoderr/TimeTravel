d149dbc257c2c3c00465ea0fe8ca460c11757257
Kartik <kkartiktummawar@gmail.com>
2025-01-05 10:37:34 +0530

    testing

--- backend/helpers/multer.js
+++ backend/helpers/multer.js
@@ -2,6 +2,11 @@
 const multer = require("multer");
 const path = require("path");
 
+const helloWorld = function(){
+  console.log("testing");
+  
+}
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads");
